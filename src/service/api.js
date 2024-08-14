83cf08236482eb809e4a51bf132161f9d3c5684b
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-08-14 21:27:38 +0300

    third

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 // import ApiKey from "./ApiKey.json";
 
 export const fetchTrends = async () => {
-  const key = "deba1855b6d5386860a66703093746c5";
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTM0NGZjYTZkZGQ0MDkwZDFkNzZhZGEwZGI1ZGNjNCIsIm5iZiI6MTcyMjA3OTUzMC40NTgyMjUsInN1YiI6IjY2YTRkNDQxODFlMDczZDM5MTQ0NzI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FZMxS-LpcFg4PgrInFqjQFqx2O5m-HtiQ4dTaMEVjM";
   const BaseURL =
     "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
   const response = axios.get(BaseURL, {
@@ -14,7 +15,8 @@ export const fetchTrends = async () => {
 };
 
 export const fetchDetails = async (movieId) => {
-  const key = "deba1855b6d5386860a66703093746c5";
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTM0NGZjYTZkZGQ0MDkwZDFkNzZhZGEwZGI1ZGNjNCIsIm5iZiI6MTcyMjA3OTUzMC40NTgyMjUsInN1YiI6IjY2YTRkNDQxODFlMDczZDM5MTQ0NzI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FZMxS-LpcFg4PgrInFqjQFqx2O5m-HtiQ4dTaMEVjM";
   const BaseURL = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US'`;
 
   const response = axios.get(BaseURL, {
@@ -26,7 +28,8 @@ export const fetchDetails = async (movieId) => {
 };
 
 export const fetchCredits = async (movieId) => {
-  const key = "deba1855b6d5386860a66703093746c5";
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTM0NGZjYTZkZGQ0MDkwZDFkNzZhZGEwZGI1ZGNjNCIsIm5iZiI6MTcyMjA3OTUzMC40NTgyMjUsInN1YiI6IjY2YTRkNDQxODFlMDczZDM5MTQ0NzI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FZMxS-LpcFg4PgrInFqjQFqx2O5m-HtiQ4dTaMEVjM";
   const BaseURL = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US'`;
 
   const response = axios.get(BaseURL, {
@@ -37,7 +40,8 @@ export const fetchCredits = async (movieId) => {
   return response;
 };
 export const fetchReviews = async (movieId) => {
-  const key = "deba1855b6d5386860a66703093746c5";
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTM0NGZjYTZkZGQ0MDkwZDFkNzZhZGEwZGI1ZGNjNCIsIm5iZiI6MTcyMjA3OTUzMC40NTgyMjUsInN1YiI6IjY2YTRkNDQxODFlMDczZDM5MTQ0NzI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FZMxS-LpcFg4PgrInFqjQFqx2O5m-HtiQ4dTaMEVjM";
   const BaseURL = `https://api.themoviedb.org/3/movie/${movieId}/reviews?language=en-US'`;
 
   const response = axios.get(BaseURL, {
@@ -48,7 +52,8 @@ export const fetchReviews = async (movieId) => {
   return response;
 };
 export const fetchSearch = async (query) => {
-  const key = "deba1855b6d5386860a66703093746c5";
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTM0NGZjYTZkZGQ0MDkwZDFkNzZhZGEwZGI1ZGNjNCIsIm5iZiI6MTcyMjA3OTUzMC40NTgyMjUsInN1YiI6IjY2YTRkNDQxODFlMDczZDM5MTQ0NzI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FZMxS-LpcFg4PgrInFqjQFqx2O5m-HtiQ4dTaMEVjM";
   const BaseURL = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
 
   const response = axios.get(BaseURL, {
